37ad972eabf34c7e6d954438a4e6bbba5d7fbe07
TimKlense <timklense@yahoo.com>
2022-12-05 11:35:07 -0600

    Update my_second_dbt_model.sql

--- models/example/my_second_dbt_model.sql
+++ models/example/my_second_dbt_model.sql
@@ -1,6 +1,15 @@
 
 -- Use the `ref` function to select from other models -- edit1
 
+-- edit2
+
+-- edit2
+
+-- edit2
+
 select *
 from {{ ref('my_first_dbt_model') }}
 where id = 1
+-- edit2
+
+-- edit2
